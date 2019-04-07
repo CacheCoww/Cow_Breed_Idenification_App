1bd6d7c61050f8c96eaebf9a92257dd9602b1df3
hmsmith5 <hmsmith5@illinois.edu>
2019-04-06 20:29:54 -0700

    Test of AutoML Vision API using JSON POST request and access token from Gcloud

--- app/src/main/java/com/example/cowpoopingapp/MainActivity.java
+++ app/src/main/java/com/example/cowpoopingapp/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.cowpoopingapp;
 
-import android.util.Base64;
 import android.Manifest;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import android.text.Editable;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -32,20 +26,27 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URI;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
-import static android.widget.Toast.LENGTH_LONG;
+//"ya29.GlvjBiBAyFZJfrRbeZz4qYln57CqQex95tJxSW-rhV__9qemp3hFuWM_CeMdYpYLmSpblnqOCHW\n" +
+//        "sjwlHsp9OE3cp4hmmpjrExfghMhYkHeCtwoBmJDC1VJ9fxvqw";
 
-//API Key : AIzaSyBnnTQAZ35okyLxeiHf8b76OfDE_30rEtk
-//HeEeEeEyYy SqUiDwArD
 
 public class MainActivity extends AppCompatActivity {
+
+private static final String accesstoken = "ya29.c.El_kBuMlQ-rCPJLIISVmGszfu3iBRrWLd2oJAkoYGb8NUsGEkl1IrmVeOkadtzH9zIlmy3V-kfarnCAdiHEeWOgMikOo8keb4lIePNQLfxEarRDzFqRODvXkMkMdl49DnA";
 
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "BreedApp:Main";
@@ -68,10 +69,10 @@ public class MainActivity extends AppCompatActivity {
         //GUI Elements
         final TextView myTextView = findViewById(R.id.BreedText);
         final EditText imageURI;
-        imageURI = findViewById(R.id.enterURL);
+       // imageURI = findViewById(R.id.enterURL);
         final Button button = findViewById(R.id.button);
         ImageView viewImage;
-        Button b;
+    /*    Button b;
         b=(Button)findViewById(R.id.btnSelectPhoto);
         viewImage=(ImageView)findViewById(R.id.viewImage);
         b.setOnClickListener(new View.OnClickListener() {
@@ -79,15 +80,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectImage();
             }
-        });
+        });*/
 
 
         //Toggle buttons:
         final Button button_upload = findViewById(R.id.toggleButton);
         final Button button_url = findViewById(R.id.toggleButton1);
 
-        //Show either URL text field or upload field and Enter button on button click
-        button_url.setOnClickListener(new View.OnClickListener() {
+        //Toggle buttons to Show either URL text field or upload field and Enter button on button click
+ /*       button_url.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imageURI.setVisibility(View.VISIBLE);
@@ -102,10 +103,14 @@ public class MainActivity extends AppCompatActivity {
                 button.setVisibility(View.VISIBLE);
                 button_type = 0;
             }
-        });
-
+        });*/
+//Button to make API call after image upload method is selected
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                autoMLTest("https://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23012_beagle.jpg");
+                final TextView myTextView = (TextView) findViewById(R.id.BreedText);
+                myTextView.setText("Moo");
+                /*
                 if (button_type == 0 && bitmap != null) {
                     try {
                         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -123,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
                     //Editable textTest = imageURI.getText();
                     String testcow = imageURI.getText().toString();
                     myTextView.setText(testcow);
-                }
+                } */
                 }
 
         });
@@ -133,8 +138,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
-
-    private void selectImage() {
+//Method to select image from Phones photo gallery
+   private void selectImage() {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         //Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -145,8 +150,8 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, IMAGE_GALLERY_REQUEST);
 
     }
-
-    Bitmap bitmap;
+    //Deal with image after it is selected from Gallery
+   Bitmap bitmap;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
@@ -170,8 +175,90 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Testing the newly created AUTOML Model on Google Cloud
+    //Creates JSON object from Holstein cow image on the web
+    //Attempts to POST to REST API
+    private void autoMLTest(String url) {
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+            StrictMode.setThreadPolicy(policy);
+            //url = "https://www.iof2020.eu/trials/use-cases/image-thumb__246__auto_b86924d5bcb568ec4e39ada86f2ae768/2.2.png";
+            URL myurl = new URL("https://cdn1-www.dogtime.com/assets/uploads/2011/01/file_23012_beagle.jpg");
+            HttpURLConnection connection = (HttpURLConnection) myurl.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmapImage = BitmapFactory.decodeStream(input);
+            ImageView viewImage;
+            viewImage = (ImageView) findViewById(R.id.viewImage);
+            viewImage.setImageBitmap(bitmapImage);
+            //Uri myUri = Uri.parse(url);
+            //InputStream inputStream = getContentResolver().openInputStream(myUri);
+            //Bitmap bitmapImage = BitmapFactory.decodeStream(inputStream);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            String base64webimage = Base64.encodeToString(byteArray, Base64.NO_WRAP);
+
+            JSONObject JSONObjPayload = new JSONObject();
+            JSONObject JSONObjInner = new JSONObject();
+            JSONObject JSONObjImage = new JSONObject();
+            //JSONObject JSONObjOverall = new JSONObject();
+
+            JSONObjImage.put("imageBytes", base64webimage);
+            JSONObjInner.put("image", JSONObjImage);
+            JSONObjPayload.put("payload", JSONObjInner);
+            //JSONObjOverall.put("request", JSONObjPayload);
+
+            final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
+                    "https://automl.googleapis.com/v1beta1/projects/animal-identification-app/locations/us-central1/models/ICN6662254112112983311:predict", JSONObjPayload,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            apiTestCallDone(response);
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            })
+
+            {
 
 
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError{
+                    Map<String, String> headers = new HashMap<>();
+                    //headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", "Bearer " + accesstoken);
+                    return headers;
+                }
+
+            };
+
+            jsonObjectRequest.setShouldCache(false);
+            requestQueue.add(jsonObjectRequest);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    void apiTestCallDone(JSONObject response){
+        try {
+            Log.d(TAG, response.toString(2));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //API call to the Google Vision API using either an image URL from web or uploaded image from camera
     void startAPICall(final String imageurl) {
         try {
             //JSON data needed for Google Vision API
@@ -182,7 +269,7 @@ public class MainActivity extends AppCompatActivity {
             JSONObject JSONObjImagesrc = new JSONObject();
             JSONArray JSONArrayFeature = new JSONArray();
             JSONObject JSONObjFeature = new JSONObject();
-            JSONObject JSONBase64img = new JSONObject();
+            //JSONObject JSONBase64img = new JSONObject();
 
             //Arranging JSON data into proper format
             JSONObjImagesrc.put("imageUri", imageurl);
@@ -231,6 +318,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Response from Google Vision API
+    //Returns first match
     void apiCallDone(JSONObject response) {
         try {
             //Parse JSON Response
@@ -258,50 +347,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
 }
-
-
-//UNUSED CODE SNIPPETS:
-
-
-        /*
-        super.onActivityResult(2, RESULT_OK, intent);
-        Uri selectedImage = intent.getData();
-        String[] filePath = { MediaStore.Images.Media.DATA };
-        Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
-        c.moveToFirst();
-        int columnIndex = c.getColumnIndex(filePath[0]);
-        String picturePath = c.getString(columnIndex);
-        c.close();
-        Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-        Log.w("path of image", picturePath+"");
-        ImageView viewImage;
-        viewImage=(ImageView)findViewById(R.id.viewImage);
-        viewImage.setImageBitmap(thumbnail);
-
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-byte[] byteArray = byteArrayOutputStream .toByteArray();
-    } */
-
-
-        /* COW BREED TRAINING in AUTOML
-        Cow Breeds to Test for:
-        1. Holstein
-        2. Jersey
-        3. Angus
-        4. Hereford
-        5. Charolais
-        6. Simmental
-        7. Ayrshire
-        8. Brown Swiss
-        9. Texas Longhorn
-        10. Brahman
-
-
-
-
-
-        */
-
 
--- app/src/test/java/com/example/cowpoopingapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/cowpoopingapp/ExampleUnitTest.java
@@ -15,3 +15,57 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 }
+
+
+
+//UNUSED CODE SNIPPETS:
+        /*
+        super.onActivityResult(2, RESULT_OK, intent);
+        Uri selectedImage = intent.getData();
+        String[] filePath = { MediaStore.Images.Media.DATA };
+        Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+        c.moveToFirst();
+        int columnIndex = c.getColumnIndex(filePath[0]);
+        String picturePath = c.getString(columnIndex);
+        c.close();
+        Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+        Log.w("path of image", picturePath+"");
+        ImageView viewImage;
+        viewImage=(ImageView)findViewById(R.id.viewImage);
+        viewImage.setImageBitmap(thumbnail);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+byte[] byteArray = byteArrayOutputStream .toByteArray();
+    } */
+
+        /* COW BREED TRAINING in AUTOML
+        Cow Breeds to Test for:
+       May add more cow breeds in the future.
+        1. Holstein
+        2. Jersey
+        3. Angus
+        4. Hereford
+        5. Charolais
+        6. Simmental
+        7. Texas Longhorn
+        8. Red Aungus
+
+        */
+
+
+//Vision API TEST:
+//API Key : AIzaSyBnnTQAZ35okyLxeiHf8b76OfDE_30rEtk
+//HeEeEeEyYy SqUiDwArD
+
+//AutoML Vision
+//POST URL: https://automl.googleapis.com/v1beta1/projects/animal-identification-app/locations/us-central1/models/ICN6662254112112983311:predict
+/* JSON format
+{
+  "payload": {
+    "image": {
+      "imageBytes": "YOUR_IMAGE_BYTE"
+    }
+  }
+}
+ */
