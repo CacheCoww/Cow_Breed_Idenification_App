6903a2b84280e24f92ef61f86740d6700daac5a1
hmsmith5 <hmsmith5@illinois.edu>
2020-04-24 19:34:55 -0700

    Changed AutoML API to TFLite model

--- app/src/main/java/com/cowbreedidentifier/cowpoopingapp/DisplayResult.java
+++ app/src/main/java/com/cowbreedidentifier/cowpoopingapp/DisplayResult.java
@@ -1,10 +1,13 @@
 package com.cowbreedidentifier.cowpoopingapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.StrictMode;
@@ -13,6 +16,7 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -36,6 +40,7 @@ import org.json.JSONObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -47,17 +52,27 @@ import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.tensorflow.lite.support.model.Model;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.label.TensorLabel;
 
 public class DisplayResult extends AppCompatActivity {
     private static final String TAG = "BreedApp:Main";
     private static RequestQueue requestQueue;
     private int button_type;
     String base64WebImage;
-    protected Interpreter tflite;
-
+    String breedresult;
 
     static {
         System.loadLibrary("native-lib");
@@ -72,13 +87,11 @@ public class DisplayResult extends AppCompatActivity {
         final String cowURL = intent.getStringExtra(MainActivity.EXTRA_URL);
         button_type = intent.getIntExtra(MainActivity.EXTRA_INT, 0);
 
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-    //Get image from main activity and Display image on screen
+        //Get image from main activity and Display image on screen
         //3 functions corresponding to where image came from to run Cow ML Model
-
-
         if (button_type == 0) {
             displayCameraImage(cowURL);
         }
@@ -89,10 +102,9 @@ public class DisplayResult extends AppCompatActivity {
             displayWebImage(cowURL);
         }
 
-    //Start Over button to go back to main screen/activity
+        //Start Over button to go back to main screen/activity
         final Button startOver = (Button) findViewById(R.id.startOver);
         startOver.setVisibility(View.VISIBLE);
-
         startOver.setOnClickListener(new View.OnClickListener(){
         @Override
             public void onClick(View v){
@@ -102,10 +114,15 @@ public class DisplayResult extends AppCompatActivity {
             }
         });
 
-
     }
 
+    //Image
     Bitmap bitmap;
+
+    /**
+     *Function that displays Camera image on screen and makes subsequent call to ML API
+     * @params cowURL: location of image
+     */
     void displayCameraImage(String cowURL) {
         try {
             Uri cowURI = Uri.parse(cowURL);
@@ -120,13 +137,21 @@ public class DisplayResult extends AppCompatActivity {
             MLTest(base64WebImage);
             final Button startOver = (Button)findViewById(R.id.startOver);
             startOver.setVisibility(View.VISIBLE);
+
         } catch (Exception e) {
             e.printStackTrace();
+            final TextView defaultTextView = findViewById(R.id.defaultInstructions);
+            defaultTextView.setVisibility(View.VISIBLE);
+            defaultTextView.setText("An unexpected error occurred. Please try again");
+            final Button startOver = (Button) findViewById(R.id.startOver);
+            startOver.setVisibility(View.VISIBLE);
         }
-
     }
 
-
+    /**
+     *Function that displays Camera gallery image on screen and makes subsequent call to ML API
+     * @params cowURL: location of image
+     */
     void displayGalleryImage(String cowURL) {
         try {
             InputStream inputStream;
@@ -154,7 +179,10 @@ public class DisplayResult extends AppCompatActivity {
 
     }
 
-
+    /**
+     *Function that displays image from web URL and makes subsequent call to ML API
+     * @params cowURL: location of image
+     */
     void displayWebImage(String cowURL) {
         try {
             URL myURL = new URL(cowURL);
@@ -175,6 +203,11 @@ public class DisplayResult extends AppCompatActivity {
 
         } catch (Exception e) {
             e.printStackTrace();
+            final TextView defaultTextView = findViewById(R.id.defaultInstructions);
+            defaultTextView.setVisibility(View.VISIBLE);
+            defaultTextView.setText("An unexpected error occurred. Please try again");
+            final Button startOver = (Button) findViewById(R.id.startOver);
+            startOver.setVisibility(View.VISIBLE);
         }
 
     }
@@ -190,105 +223,111 @@ public class DisplayResult extends AppCompatActivity {
     }
 
     String getModelPath(){
-        return "file:///android_asset/model.tflite";
+        return "model.tflite";
     }
+
+
     /**
-     * Function for performing the cow breed identification using an image in base64 format. If the function fails to identify a cow
+     * Function for performing the cow breed identification using an image in base64 format and the TFLite predication model. If the function fails to identify a cow
      * then the image is routed to the Vision Function call which performs a generic identification of the image.
      * @param base64WebImage
      */
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     void MLTest(String base64WebImage){
+
+        //Prepare image for TFLite model
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+                        .build();
+        TensorImage tImage = new TensorImage(DataType.UINT8);
+
+        TensorBuffer probabilityBuffer =
+                TensorBuffer.createFixedSize(new int[]{1, 16}, DataType.UINT8);
+
         try{
-        String sampleCowImage = stringFromImage();
-        byte[] bytez = sampleCowImage.getBytes();
-        String goat = new String(Base64.decode(bytez, Base64.DEFAULT));
-        InputStream is = new ByteArrayInputStream(goat.getBytes(Charset.forName("UTF-8")));
+            byte[] decodedString = Base64.decode(base64WebImage, Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        GoogleCredentials cowImageRep = GoogleCredentials.fromStream(is).createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
-        cowImageRep.refreshIfExpired();
-        AccessToken accesstoken = cowImageRep.getAccessToken();
-        final String token = accesstoken.getTokenValue();
-        is.close();
+            tImage.load(decodedByte);
+            tImage = imageProcessor.process(tImage);
 
-        //Set up JSON Object Payload to send to API
-        JSONObject JSONObjPayload = new JSONObject();
-        JSONObject JSONObjInner = new JSONObject();
-        JSONObject JSONObjImage = new JSONObject();
 
-        JSONObjImage.put("imageBytes", base64WebImage);
-        JSONObjInner.put("image", JSONObjImage);
-        JSONObjPayload.put("payload", JSONObjInner);
+            MappedByteBuffer tfliteModel
+                    = FileUtil.loadMappedFile(this,
+                    "model.tflite");
+            @Deprecated
+            Interpreter tflite = new Interpreter(loadModelFile(this));
 
-        try (Interpreter interpreter = new Interpreter(file_of_a_tensorflowlite_model)) {
-            interpreter.run(input, output);
+            if(null != tflite) {
+                tflite.run(tImage.getBuffer(), probabilityBuffer.getBuffer());
+            }
+
+        } catch (IOException e){
+           // Log.e("tfliteSupport", "Error reading model", e);
         }
 
-        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,
-                "https://automl.googleapis.com/v1beta1/projects/animal-identification-app/locations/us-central1/models/ICN7163460716149905373:predict", JSONObjPayload,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(final JSONObject response) {
-                        MLTestResponse(response);
+        final String ASSOCIATED_AXIS_LABELS = "dict.txt";
+        List<String> associatedAxisLabels = null;
+        try {
+            associatedAxisLabels = FileUtil.loadLabels(this, ASSOCIATED_AXIS_LABELS);
+        } catch (IOException e) {
+            //Log.e("tfliteSupport", "Error reading label file", e);
+            final TextView defaultTextView = findViewById(R.id.defaultInstructions);
+            defaultTextView.setVisibility(View.VISIBLE);
+            defaultTextView.setText("An unexpected error occurred. Please try again");
+            final Button startOver = (Button) findViewById(R.id.startOver);
+            startOver.setVisibility(View.VISIBLE);
+        } catch (Exception e){
+            final TextView defaultTextView = findViewById(R.id.defaultInstructions);
+            defaultTextView.setVisibility(View.VISIBLE);
+            defaultTextView.setText("An unexpected error occurred. Please try again");
+            final Button startOver = (Button) findViewById(R.id.startOver);
+            startOver.setVisibility(View.VISIBLE);
+        }
 
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(final VolleyError error) {
-                //Log.e(TAG, error.toString());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Authorization", "Bearer " + token);
-                return headers;
-            }
-        };
-        jsonObjectRequest.setRetryPolicy(new RetryPolicy() {
-            @Override
-            public int getCurrentTimeout() {
-                return 50000;
-            }
-            @Override
-            public int getCurrentRetryCount() {
-                return 50000;
-            }
-            @Override
-            public void retry(VolleyError error) throws VolleyError {
-            }
-        });
+        TensorProcessor probabilityProcessor =
+                new TensorProcessor.Builder().add(new NormalizeOp(0, 255)).build();
 
-        jsonObjectRequest.setShouldCache(false);
-        requestQueue.add(jsonObjectRequest);
+        if (null != associatedAxisLabels) {
+            // Map of labels and their corresponding probability
+            TensorLabel labels = new TensorLabel(associatedAxisLabels,
+                    probabilityProcessor.process(probabilityBuffer));
 
-    } catch (Exception e) {
+            // Create a map to access the result based on label
+            Map<String, Float> floatMap = labels.getMapWithFloatValue();
+
+            float maximum = 0;
+            String breed = "None";
+            for (Map.Entry<String,Float> entry : floatMap.entrySet()){
+                float val = entry.getValue();
+                if (val > maximum){
+                    maximum = val;
+                    breed = entry.getKey();
+                }
+            }
+            MLTestResponse(breed, maximum);
+           // Log.d("map", floatMap.toString());
+        }
+
+
+        /*catch (Exception e) {
             final TextView defaultTextView = findViewById(R.id.defaultInstructions);
             defaultTextView.setVisibility(View.VISIBLE);
             defaultTextView.setText("An unexpected error occurred. Please try again");
             final Button startOver = (Button)findViewById(R.id.startOver);
             startOver.setVisibility(View.VISIBLE);
         e.printStackTrace();
-    }
+    } */
 }
 
-
+//https://github.com/tensorflow/tensorflow/blob/master/tensorflow/lite/experimental/support/java/README.md
     /**
      * Function that parses JSON response from ML model and returns cow breed as String displayed on screen
-     * @param response
+     * @param breed score
      */
-    void MLTestResponse(JSONObject response){
+    void MLTestResponse(String breed, float score){
         try {
-            JSONObject newResponse = response;
-            //Log.d(TAG, newResponse.toString(2)); //Test response from API
-            JSONArray arrRet = response.getJSONArray("payload");
-            JSONObject objRet = (JSONObject) arrRet.get(0);
-            String displayName = (String) objRet.get("displayName");
-            JSONObject classification = (JSONObject) objRet.getJSONObject("classification");
-            double score = (double) classification.get("score");
-            //Log.d(TAG, Double.toString(score));
-
-            if (score > 0.50) {
+            if (score > 0.40 || checkInternetConnectivity() == false) {
                 final TextView myTextView = (TextView) findViewById(R.id.BreedText);
                 final TextView defaultTextView = findViewById(R.id.defaultInstructions);
                 defaultTextView.setVisibility(View.VISIBLE);
@@ -311,25 +350,26 @@ public class DisplayResult extends AppCompatActivity {
                 breedMap.put("gelbvieh", "Gelbvieh");
                 breedMap.put("guernsey", "Guernsey");
                 breedMap.put("shorthorn", "Short horn");
-                myTextView.setText(breedMap.get(displayName));
+                if (breedMap.containsKey(breed)){
+                    breed = breedMap.get(breed);
+                }
+                breedresult = breed;
+                myTextView.setText(breed);
                 myTextView.setVisibility(View.VISIBLE);
                 final Button startOver = (Button) findViewById(R.id.startOver);
                 startOver.setVisibility(View.VISIBLE);
             } else {
-                //Score is less than 0.50- probably not a cow, only works for URLs currently
                 startVisionCall(base64WebImage);
-
             }
 
-        } catch (JSONException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-            //Log.d(TAG, "An Unexpected Error occurred");
-                startVisionCall(base64WebImage);
-                final Button startOver = (Button) findViewById(R.id.startOver);
-                startOver.setVisibility(View.VISIBLE);
-
+            final TextView defaultTextView = findViewById(R.id.defaultInstructions);
+            defaultTextView.setVisibility(View.VISIBLE);
+            defaultTextView.setText("An unexpected error occurred. Please try again");
+            final Button startOver = (Button) findViewById(R.id.startOver);
+            startOver.setVisibility(View.VISIBLE);
         }
-
     }
 
     /**
@@ -462,6 +502,19 @@ public class DisplayResult extends AppCompatActivity {
             final Button startOver = (Button)findViewById(R.id.startOver);
             startOver.setVisibility(View.VISIBLE);
         }
+    }
+
+    boolean checkInternetConnectivity(){
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network
+            connected = true;
+        }
+        else
+            connected = false;
+        return connected;
     }
 
     public native String stringFromImage();
